918e79100fbe47e2b3bed3ec5b2b042e003d9bf6
sanket0610 <sanketkingz10@gmail.com>
2025-07-28 15:14:44 +0530

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,10 @@
+
+n1=input("Enter number 1 ")
+n2=input("Enter number 2 ")
+n3=input("Enter number 3 ")
+
+print("Before swapping the numbers:",n1 ,n2 ,n3)
+
+n1,n2,n3=n3,n2,n1
+
+print("After swapping the numbers:",n1,n2,n3)
